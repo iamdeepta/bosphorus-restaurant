75b5eb0b539b24f23717703c5deef607e70af532
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2021-11-04 20:42:50 +0600

    updated

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -2,8 +2,195 @@ import React from "react";
 import "./css/checkout.css";
 import AppUrl from "../../classes/AppUrl";
 //import { Link } from "react-router-dom";
+import { FcMoneyTransfer } from "react-icons/fc";
+import swal from "sweetalert";
 
 const Checkout = () => {
+  function activeGuest() {
+    let guest_btn = document.querySelector(
+      ".checkout_left_content_login_guest_text"
+    );
+    let user_btn = document.querySelector(
+      ".checkout_left_content_login_user_text"
+    );
+    let welcome_msg = document.querySelector(
+      ".checkout_left_content_login_body"
+    );
+    let login_ui = document.querySelector(
+      ".checkout_left_content_login_form_div"
+    );
+    let shipping_ui = document.querySelector(
+      ".checkout_left_content_shipping_div"
+    );
+    let login_header = document.querySelector(".checkout_left_content_card");
+    let shipping_header = document.querySelector(
+      ".checkout_left_content_card_shipping"
+    );
+    guest_btn.classList.add("checkout_left_content_login_guest_text_active");
+    user_btn.classList.remove("checkout_left_content_login_user_text_active");
+    welcome_msg.classList.add("checkout_left_content_login_body_inactive");
+    login_ui.classList.remove("checkout_left_content_login_form_div_active");
+    shipping_ui.classList.add("checkout_left_content_shipping_div_active");
+
+    login_header.style.display = "none";
+    shipping_header.style.display = "flex";
+  }
+
+  function activeUser() {
+    let user_btn = document.querySelector(
+      ".checkout_left_content_login_user_text"
+    );
+    let guest_btn = document.querySelector(
+      ".checkout_left_content_login_guest_text"
+    );
+    let welcome_msg = document.querySelector(
+      ".checkout_left_content_login_body"
+    );
+    let login_ui = document.querySelector(
+      ".checkout_left_content_login_form_div"
+    );
+    let shipping_ui = document.querySelector(
+      ".checkout_left_content_shipping_div"
+    );
+    user_btn.classList.add("checkout_left_content_login_user_text_active");
+    guest_btn.classList.remove("checkout_left_content_login_guest_text_active");
+    welcome_msg.classList.add("checkout_left_content_login_body_inactive");
+    login_ui.classList.add("checkout_left_content_login_form_div_active");
+    shipping_ui.classList.remove("checkout_left_content_shipping_div_active");
+  }
+
+  function backToWelcomeMsg() {
+    let shipping_ui = document.querySelector(
+      ".checkout_left_content_shipping_div"
+    );
+    let welcome_msg = document.querySelector(
+      ".checkout_left_content_login_body"
+    );
+    let login_header = document.querySelector(".checkout_left_content_card");
+    let shipping_header = document.querySelector(
+      ".checkout_left_content_card_shipping"
+    );
+    let guest_btn = document.querySelector(
+      ".checkout_left_content_login_guest_text"
+    );
+
+    shipping_ui.classList.remove("checkout_left_content_shipping_div_active");
+    welcome_msg.classList.remove("checkout_left_content_login_body_inactive");
+    guest_btn.classList.remove("checkout_left_content_login_guest_text_active");
+    login_header.style.display = "flex";
+    shipping_header.style.display = "none";
+  }
+
+  function nextToOtp() {
+    let shipping_ui = document.querySelector(
+      ".checkout_left_content_shipping_div"
+    );
+    let otp_ui = document.querySelector(".checkout_left_content_otp_form_div");
+    let otp_header_letter = document.querySelector(
+      ".checkout_left_content_login_a_shipping"
+    );
+    let otp_header = document.querySelector(
+      ".checkout_left_content_login_div_shipping p"
+    );
+
+    shipping_ui.classList.remove("checkout_left_content_shipping_div_active");
+    otp_ui.classList.add("checkout_left_content_otp_form_div_active");
+    otp_header_letter.innerHTML = "c";
+    otp_header.innerHTML = "OTP CONFIRMATION";
+  }
+
+  function backToShipping() {
+    let shipping_ui = document.querySelector(
+      ".checkout_left_content_shipping_div"
+    );
+    let otp_ui = document.querySelector(".checkout_left_content_otp_form_div");
+    let otp_header_letter = document.querySelector(
+      ".checkout_left_content_login_a_shipping"
+    );
+    let otp_header = document.querySelector(
+      ".checkout_left_content_login_div_shipping p"
+    );
+
+    shipping_ui.classList.add("checkout_left_content_shipping_div_active");
+    otp_ui.classList.remove("checkout_left_content_otp_form_div_active");
+    otp_header_letter.innerHTML = "b";
+    otp_header.innerHTML = "SHIPPING ADDRESS";
+  }
+
+  function nextToPaymentMethod() {
+    let pm_ui = document.querySelector(".checkout_left_content_payment_method");
+    let otp_ui = document.querySelector(".checkout_left_content_otp_form_div");
+    let pm_header_letter = document.querySelector(
+      ".checkout_left_content_login_a_shipping"
+    );
+    let pm_header = document.querySelector(
+      ".checkout_left_content_login_div_shipping p"
+    );
+
+    pm_ui.classList.add("checkout_left_content_payment_method_active");
+    otp_ui.classList.remove("checkout_left_content_otp_form_div_active");
+    pm_header_letter.innerHTML = "d";
+    pm_header.innerHTML = "PAYMENT METHOD";
+  }
+
+  function backToOtp() {
+    let payment_ui = document.querySelector(
+      ".checkout_left_content_payment_method"
+    );
+    let otp_ui = document.querySelector(".checkout_left_content_otp_form_div");
+    let otp_header_letter = document.querySelector(
+      ".checkout_left_content_login_a_shipping"
+    );
+    let otp_header = document.querySelector(
+      ".checkout_left_content_login_div_shipping p"
+    );
+
+    payment_ui.classList.remove("checkout_left_content_payment_method_active");
+    otp_ui.classList.add("checkout_left_content_otp_form_div_active");
+    otp_header_letter.innerHTML = "c";
+    otp_header.innerHTML = "OTP CONFIRMATION";
+  }
+
+  function bkashMethod() {
+    let bkash_btn = document.querySelector(
+      ".checkout_left_content_payment_method_bkash"
+    );
+    let user_bcod_btn = document.querySelector(
+      ".checkout_left_content_payment_method_cod"
+    );
+
+    bkash_btn.classList.add(
+      "checkout_left_content_payment_method_bkash_active"
+    );
+    user_bcod_btn.classList.remove(
+      "checkout_left_content_payment_method_cod_active"
+    );
+  }
+
+  function codMethod() {
+    let bkash_btn = document.querySelector(
+      ".checkout_left_content_payment_method_bkash"
+    );
+    let user_bcod_btn = document.querySelector(
+      ".checkout_left_content_payment_method_cod"
+    );
+
+    bkash_btn.classList.remove(
+      "checkout_left_content_payment_method_bkash_active"
+    );
+    user_bcod_btn.classList.add(
+      "checkout_left_content_payment_method_cod_active"
+    );
+  }
+
+  function paymentConfirm() {
+    swal({
+      title: "Order has been placed successfully",
+      text: "Thank you for purchasing from us.",
+      icon: "success",
+    });
+  }
+
   return (
     <>
       <section className="checkout_section container">
@@ -15,16 +202,199 @@ const Checkout = () => {
             <div className="checkout_left_content_card">
               <span className="checkout_left_content_login_a">a</span>
               <div className="checkout_left_content_login_div">
-                <p className="checkout_left_content_login_guest_text">Guest</p>
-                <p className="checkout_left_content_login_user_text">Login</p>
+                <p
+                  className="checkout_left_content_login_guest_text"
+                  onClick={() => activeGuest()}
+                >
+                  Guest
+                </p>
+                <p
+                  className="checkout_left_content_login_user_text"
+                  onClick={() => activeUser()}
+                >
+                  Login
+                </p>
               </div>
             </div>
 
-            <div className="checkout_left_content_login_body">
-              <p className="checkout_left_content_login_body_text">
-                Confirm your order as a Guest or Login to your account.
-              </p>
+            <div className="checkout_left_content_card_shipping">
+              <span className="checkout_left_content_login_a_shipping">b</span>
+              <div className="checkout_left_content_login_div_shipping">
+                <p>SHIPPING ADDRESS</p>
+              </div>
             </div>
+
+            <div className="checkout_left_content_user_body_div">
+              <div className="checkout_left_content_login_body">
+                <p className="checkout_left_content_login_body_text">
+                  Confirm your order as a Guest or Login to your account.
+                </p>
+              </div>
+
+              <div className="checkout_left_content_login_form_div">
+                <div className="checkout_left_content_login_form_content">
+                  <div className="checkout_left_content_login_form_left_content">
+                    <p className="checkout_left_content_login_form_left_content_name_text">
+                      Email*
+                    </p>
+                    <input
+                      type="text"
+                      className="checkout_left_content_login_form_email"
+                    />
+                  </div>
+
+                  <div className="checkout_left_content_login_form_right_content">
+                    <p className="checkout_left_content_login_form_right_content_name_text">
+                      Password*
+                    </p>
+                    <input
+                      type="text"
+                      className="checkout_left_content_login_form_password"
+                    />
+                  </div>
+                </div>
+                <div className="checkout_left_content_login_btn">Login</div>
+              </div>
+
+              <div className="checkout_left_content_shipping_div">
+                <div className="checkout_left_content_shipping_form_content">
+                  <div className="checkout_left_content_shipping_form_left_content">
+                    <p className="checkout_left_content_shipping_form_left_content_name_text">
+                      Name*
+                    </p>
+                    <input
+                      type="text"
+                      className="checkout_left_content_shipping_form_name"
+                    />
+
+                    <p className="checkout_left_content_shipping_form_left_content_email_text">
+                      Email*
+                    </p>
+                    <input
+                      type="text"
+                      className="checkout_left_content_shipping_form_email"
+                    />
+
+                    <p className="checkout_left_content_shipping_form_left_content_city_text">
+                      City*
+                    </p>
+                    <input
+                      type="text"
+                      className="checkout_left_content_shipping_form_city"
+                    />
+                  </div>
+
+                  <div className="checkout_left_content_shipping_form_right_content">
+                    <p className="checkout_left_content_shipping_form_right_content_phone_text">
+                      Phone*
+                    </p>
+                    <input
+                      type="text"
+                      className="checkout_left_content_shipping_form_phone"
+                    />
+
+                    <p className="checkout_left_content_shipping_form_right_content_address_text">
+                      Address*
+                    </p>
+                    <input
+                      type="text"
+                      className="checkout_left_content_shipping_form_address"
+                    />
+
+                    <p className="checkout_left_content_shipping_form_right_content_postal_text">
+                      Postal Code*
+                    </p>
+                    <input
+                      type="text"
+                      className="checkout_left_content_shipping_form_postal"
+                    />
+                  </div>
+                </div>
+                <div className="checkout_left_content_shipping_btn_div">
+                  <div
+                    className="checkout_left_content_shipping_btn_prev"
+                    onClick={() => backToWelcomeMsg()}
+                  >
+                    Previous
+                  </div>
+                  <div
+                    className="checkout_left_content_shipping_btn_next"
+                    onClick={() => nextToOtp()}
+                  >
+                    Next
+                  </div>
+                </div>
+              </div>
+
+              <div className="checkout_left_content_otp_form_div">
+                <div className="checkout_left_content_otp_form_content">
+                  <div className="checkout_left_content_otp_form_left_content">
+                    <p className="checkout_left_content_otp_form_left_content_otp_text">
+                      OTP*
+                    </p>
+                    <input
+                      type="text"
+                      className="checkout_left_content_otp_form_otp"
+                    />
+                  </div>
+                </div>
+                <div className="checkout_left_content_otp_btn_div">
+                  <div
+                    className="checkout_left_content_otp_btn_prev"
+                    onClick={() => backToShipping()}
+                  >
+                    Previous
+                  </div>
+                  <div
+                    className="checkout_left_content_otp_btn_next"
+                    onClick={() => nextToPaymentMethod()}
+                  >
+                    Next
+                  </div>
+                </div>
+              </div>
+
+              <div className="checkout_left_content_payment_method">
+                <div className="checkout_left_content_payment_method_row">
+                  <div
+                    className="checkout_left_content_payment_method_cod"
+                    onClick={() => codMethod()}
+                  >
+                    <FcMoneyTransfer className="checkout_left_content_payment_method_cod_icon"></FcMoneyTransfer>
+                    Cash On Delivery
+                  </div>
+                  <div
+                    className="checkout_left_content_payment_method_bkash"
+                    onClick={() => bkashMethod()}
+                  >
+                    <img
+                      className="checkout_left_content_payment_method_bkash_pic"
+                      src={AppUrl.image_url + "assets/images/bkash.png"}
+                      alt="bkash"
+                    />
+                    Bkash
+                  </div>
+                </div>
+                <div className="checkout_left_content_payment_method_btn">
+                  <div
+                    className="checkout_left_content_payment_method_btn_prev"
+                    onClick={() => backToOtp()}
+                  >
+                    Previous
+                  </div>
+                  <div
+                    className="checkout_left_content_payment_method_btn_next"
+                    onClick={() => paymentConfirm()}
+                  >
+                    Confirm
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="checkout_left_content_btn_div">
+              <Link classList="checkout_left_content_login_btn">Login</Link>
+            </div> */}
           </div>
           <div className="checkout_right_content">
             <div className="checkout_cart_header_div">
